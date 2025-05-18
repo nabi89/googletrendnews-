544b67585c7869ff518faea114377c1ec1d51a87
nabi89 <nbekusmanbekov1989@gmail.com>
2025-05-18 10:22:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,13 @@ from google import genai
 from pydantic import BaseModel
 
 
-
 conn=sqlitecloud.connect('sqlitecloud://cwcgjb0ahz.g1.sqlite.cloud:8860/chinook.sqlite?apikey=DaG8uyqMPa9GdxoR7ObMoajHIdfUOrc7B0mF0IrU6Y0')
 c=conn.cursor()
 
 c.execute("SELECT * FROM haberler ORDER BY trend_id DESC LIMIT 99")
 haberler=c.fetchall()
 
-for i in range(lanhaberler):
+for i in range(haberler):
     st.image(haberler[i][3])
     st.write(haberler[i][1])
-    st.link_button("Habere git",haberler[i][2])
+    st.link_button("Habere Git",haberler[i][2])
